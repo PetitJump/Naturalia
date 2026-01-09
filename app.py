91b25d828bc76cd869a1dcc42bc7cb9f771db2a9
kyniops <cosmosyt34@gmail.com>
2026-01-09 11:20:58 +0100

    Initialize Flask app with routes for index and world

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-#Fichier pour Flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/world")
+def world():
+    return render_template('world.html')
+
+app.run(host = '127.0.0.1', port='5000', debug=True)
