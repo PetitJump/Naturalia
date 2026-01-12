41f159745bdbde1daa556e9d59ac7b792ce465f9
PetitJump <killianbonnin7@gmail.com>
2026-01-12 13:34:18 +0100

    Creation de nouvelles pages html

--- app.py
+++ app.py
@@ -3,18 +3,29 @@ from algo import update #tester() est la fonction principale (la seul utiliser p
 app = Flask(__name__)
 
 @app.route("/")
-def index():
+def index(): #Par Hugo
     return render_template("index.html")
 
-@app.route("/world")
-def world():
+@app.route("/init")
+def init(): #Par Hugo
     predateur = {"nom" : "loup", "nombres" : ...}
     proie = {"nom" : "mouton", "nombres" : ...}
     vegetal = {"nom" : "herbe", "nombres" : ...}
     jour = ... #Peut etre utiliser les sessions
     predateur, proie, vegetal = update(jour, predateur, proie, vegetal) #Va mettre a jour les datas avec les algos
 
-    return render_template('world.html')
+    return render_template('init.html')
+
+@app.route("/game")
+def game(): #Par Margaux
+    ...
+    return render_template('game.html')
+
+
+@app.route("/regles")
+def regles(): #Par Killian et Carl
+    ...
+    return render_template('regles.html')
 
 app.run(host = '127.0.0.1', port=5000, debug=True)
 
