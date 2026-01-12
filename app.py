cc7644d1ace457f5a4c586fdb14ceb70ab385faf
PetitJump <killianbonnin7@gmail.com>
2026-01-12 14:18:58 +0100

    Enhance modifier route with form data handling
    Added request handling and data processing for modifying animal and grass reproduction and consumption.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
 import json
 from algo import update #tester() est la fonction principale (la seul utiliser par flask)
 app = Flask(__name__)
@@ -28,9 +28,38 @@ def regles(): #Par Killian et Carl
     ...
     return render_template('regles.html')
     
+    
 @app.route("/modifier")
 def modifier(): #Par Killian et Carl
-    ...
+    loup_reproduction_tout_les: int = request.form['...']
+    loup_reproduction_combien: list[int] = [request.form['...'], request.form['...']]
+    loup_mange_tout_les: int = request.form['...']
+    loup_mange_combien: int = request.form['...']
+
+    mouton_reproduction_tout_les: int = request.form['...']
+    mouton_reproduction_combien: list[int] = [request.form['...'], request.form['...']]
+    mouton_mange_tout_les: int = request.form['...']
+    mouton_mange_combien: int = request.form['...']
+
+    herbe_reproduction_tout_les: int = request.form['...']
+    herbe_reproduction_combien: list[int] = [request.form['...'], request.form['...']]
+
+    data = {
+        "loup" : {
+            "reproduction" : {"tout_les" : loup_reproduction_tout_les, "nombre_de_nv_nee" : loup_reproduction_combien}, 
+            "mange" : {"qui" : "mouton", "tout_les" : loup_mange_tout_les, "combien" : loup_mange_combien}
+        },
+
+        "mouton" : {
+            "reproduction" : {"tout_les" : mouton_reproduction_tout_les, "nombre_de_nv_nee" : mouton_reproduction_combien},
+            "mange" : {"qui" : "herbe", "tout_les" : mouton_mange_tout_les, "combien" : mouton_mange_combien}
+        },
+
+        "herbe" : {
+            "reproduction" : {"tout_les" : herbe_reproduction_tout_les, "nombre_de_nv_nee" : herbe_reproduction_combien}
+        }
+}
+
     return render_template('index.html')
 
 if __name__ == '__main__':
