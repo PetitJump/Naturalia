dc7d08a72fa342766c695a8179c7004f4e700b44
PetitJump <killianbonnin7@gmail.com>
2026-02-03 10:02:24 +0100

    Ajout de la méthode naissance (qui marche) et suppression de la méthode mort qui ne marche pas

--- algoPOO.py
+++ algoPOO.py
@@ -14,58 +14,37 @@ class Vegetal:
 
 class Jeu:
     def __init__(self, predateurs: list[Predateur], proies: list[Proie], vegetaux: list[Vegetal]):
-        self.predateurs: list = predateurs
-        self.proies: list = proies
-        self.vegetaux: list = vegetaux
+        self.predateurs: list[Predateur] = predateurs
+        self.proies: list[Proie] = proies
+        self.vegetaux: list[Vegetal] = vegetaux
 
     def naissance(self, data: dict, jour: int):
         """Regarde les règles et change les variables en fonction des naissances. Augmente également l'âge."""
-        if self.predateurs:
-            nom_p = self.predateurs[0].nom
-            regle_p = data[nom_p]["reproduction"]
-            for p in self.predateurs:
-                p.age += 1
-            if jour % regle_p["tout_les"] == 0:
-                nb_bebes = regle_p["nombre_de_nv_nee"] * (len(self.predateurs) // 2)
-                for _ in range(nb_bebes):
-                    self.predateurs.append(Predateur(nom_p, 0)) 
-        if self.proies:
-            nom_pr = self.proies[0].nom
-            regle_pr = data[nom_pr]["reproduction"]
-            for pr in self.proies:
-                pr.age += 1
-            if jour % regle_pr["tout_les"] == 0:
-                nb_bebes = regle_pr["nombre_de_nv_nee"] * (len(self.proies) // 2)
-                for _ in range(nb_bebes):
-                    self.proies.append(Proie(nom_pr, 0)) 
-        if self.vegetaux:
-            nom_v = self.vegetaux[0].nom
-            regle_v = data[nom_v]["reproduction"]
-            if jour % regle_v["tout_les"] == 0 and len(self.vegetaux) < 2500:
-                nb_pousses = regle_v["nombre_de_nv_nee"] * (len(self.vegetaux) // 2)
-                for _ in range(nb_pousses):
-                    self.vegetaux.append(Vegetal(nom_v))
+        if jour % data["loup"]["reproduction"]["tout_les"] == 0: #Si il peut se reproduire en fonction des règles
+            nouveau_en_plus = data["loup"]["reproduction"]["nombre_de_nv_nee"] * (len(self.predateurs) // 2) #On prend le nombres de nouveau né et on le mutltiplie avec le nombre de couple de loup
+            for _ in range(nouveau_en_plus):
+                self.predateurs.append(Predateur("loup", 0)) #Un nouveau née d'age 0
+
+        for k in self.predateurs: #On augmente l'age
+            k.age += 1
+
+        if jour % data["mouton"]["reproduction"]["tout_les"] == 0: 
+            nouveau_en_plus = data["mouton"]["reproduction"]["nombre_de_nv_nee"] * (len(self.proies) // 2)
+            for _ in range(nouveau_en_plus):
+                self.proies.append(Proie("loup", 0)) #Un nouveau née d'age 0
+
+        for k in self.proies: #On augmente l'age
+            k.age += 1
+
+        if jour % data["herbe"]["reproduction"]["tout_les"] == 0 and len(self.vegetaux) < 2500: 
+            nouveau_en_plus = data["herbe"]["reproduction"]["nombre_de_nv_nee"] * (len(self.vegetaux) // 2)
+            for _ in range(nouveau_en_plus):
+                self.vegetaux.append(Vegetal("herbe"))
+
     
-    def mort(self, otr, jour : int, data : dict): #A faire par Marre Go
+    def mort(self, data : dict, jour : int): #A faire par Marre Go
         """Regarde les règles et change les variables en fonction des morts."""
-        self.age += 1
-
-        if jour % data[self.nom]["mange"]["tout_les"] == 0:
-            combien = data[self.nom]["mange"]["combien"]
-            animal_necessaires = self.nombres * combien
-
-        if otr.nombres >= animal_necessaires:
-            otr.nom = {"nom": otr.nom,"nombres":  otr.nombres - animal_necessaires, "age" : otr.age} 
-            """for i in range(animal_necessaires):
-                otr.age.pop()""" 
-        else:
-            animal_survivants = otr.nombres // combien
-            self.nom = {"nom": self.nom,"nombres": animal_survivants, "age" : self.age}
-            otr.nom = {"nom": otr.nom, "nombres": 0 , "age" : []} 
-            if animal_survivants <= 0:
-                self.age = []
-            """for i in range(animal_survivants):
-                self.age.pop()"""
+        ...
     
     def update(self, jour): #A faire par Carl
         """Fonction principal qui va etre utiliser par Flask"""
@@ -76,8 +55,8 @@ class Jeu:
             data = json.load(f)
         data = random_repro(data)
 
-        nv_predateur, nv_proie, nv_vegetal = self.naissance()
-        nv_predateur, nv_proie, nv_vegetal = self.mort()
+        nv_predateur, nv_proie, nv_vegetal = self.naissance(data, jour)
+        nv_predateur, nv_proie, nv_vegetal = self.mort(data, jour)
 
         if nv_vegetal["nombres"] < 10:
             nv_vegetal["nombres"] = 10
