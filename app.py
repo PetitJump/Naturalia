223ac1a6165b1bee7e405fec5782eed5d7b779ae
Carl Vernières <carlvernierespro@gmail.com>
2026-02-12 19:19:39 +0100

    utilisation des meutes et amélioration du code pour la lecture

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request
 import json
-import copy
-from algo import Predateur, Vegetal, Proie, Jeu
+from algo import Predateur, Vegetal, Proie, Meute, Jeu
 from graphique import creer_graphique
 
 app = Flask(__name__)
@@ -14,32 +13,40 @@ def index():
 def init(): 
     return render_template('init.html')
 
+
 @app.route("/update_ajouter", methods=['GET', 'POST'])
 def update_ajouter():
     global jeu, historique
     jour = int(request.form["base_jour"])
 
     for _ in range(int(request.form["loup"])):
-        jeu.predateurs.append(Predateur("loup", 0))
+        jeu.meute.predateurs.append(Predateur("loup", 0))
 
     for _ in range(int(request.form["cerf"])):
         jeu.proies.append(Proie("cerf", 0))
-
 
     for _ in range(int(request.form["herbe"])):
         jeu.vegetaux.append(Vegetal("herbe"))
 
     jeu.update(jour)
-
     jour += 1
-    
-    historique["loup"].append(len(jeu.predateurs))
+
+    historique["loup"].append(len(jeu.meute.predateurs))
     historique["cerf"].append(len(jeu.proies))
     historique["herbe"].append(len(jeu.vegetaux))
 
     graph_url = creer_graphique(historique)
 
-    return render_template('game.html', predateur=len(jeu.predateurs), jour=jour, proie=len(jeu.proies), vegetal=len(jeu.vegetaux), afficher_bouton=True, graph_url=graph_url)
+    return render_template(
+        'game.html',
+        predateur=len(jeu.meute.predateurs),
+        jour=jour,
+        proie=len(jeu.proies),
+        vegetal=len(jeu.vegetaux),
+        afficher_bouton=True,
+        graph_url=graph_url
+    )
+
 
 @app.route("/ajouter", methods=['GET', 'POST'])
 def ajouter(): 
@@ -48,62 +55,113 @@ def ajouter():
 
     graph_url = creer_graphique(historique)
 
-    return render_template('ajouter.html', predateur=len(jeu.predateurs), jour=jour, proie=len(jeu.proies), vegetal=len(jeu.vegetaux), graph_url=graph_url)
+    return render_template(
+        'ajouter.html',
+        predateur=len(jeu.meute.predateurs),
+        jour=jour,
+        proie=len(jeu.proies),
+        vegetal=len(jeu.vegetaux),
+        graph_url=graph_url
+    )
+
 
 @app.route("/game", methods=['GET', 'POST'])
 def game(): 
-    global  historique, jeu
+    global historique, jeu
     jour = int(request.form["jour"])
     
-    if jour == 0: #On init
-        predateurs = [Predateur("loup", 0) for i in range(int(request.form["loup"]))]
-        proies = [Predateur("loup", 0) for i in range(int(request.form["cerf"]))]
-        vegetaux = [Predateur("loup", 0) for i in range(int(request.form["herbe"]))]
-        jeu = Jeu(predateurs, proies, vegetaux)
+    if jour == 0:  # on init
+
+        nb_loup = int(request.form["loup"])
+        nb_cerf = int(request.form["cerf"])
+        nb_herbe = int(request.form["herbe"])
+        loups = [Predateur("loup", 0) for _ in range(nb_loup)]
+        meute = Meute(loups)
+        proies = [Proie("cerf", 0) for _ in range(nb_cerf)]
+        vegetaux = [Vegetal("herbe") for _ in range(nb_herbe)]
+
+        jeu = Jeu(meute, proies, vegetaux)
 
     jeu.update(jour)
     jour += 1
 
-    historique["loup"].append(len(jeu.predateurs))
+    historique["loup"].append(len(jeu.meute.predateurs))
     historique["cerf"].append(len(jeu.proies))
     historique["herbe"].append(len(jeu.vegetaux))
 
     graph_url = creer_graphique(historique)
 
-    return render_template('game.html', predateur=len(jeu.predateurs), jour=jour, proie=len(jeu.proies), vegetal=len(jeu.vegetaux), afficher_bouton=True, graph_url=graph_url)
+    return render_template(
+        'game.html',
+        predateur=len(jeu.meute.predateurs),
+        jour=jour,
+        proie=len(jeu.proies),
+        vegetal=len(jeu.vegetaux),
+        afficher_bouton=True,
+        graph_url=graph_url
+    )
+
 
 @app.route("/parametre")
 def regles(): 
     return render_template('parametre.html')
     
+
 @app.route("/modifier", methods=['GET', 'POST'])
 def modifier(): 
-    loup_reproduction_tout_les: int = int(request.form['nb_bebe_tout_les_preda'])
-    loup_reproduction_combien: list[int] = [int(request.form['nb_bebe_predateur1']), int(request.form['nb_bebe_predateur2'])]
-    loup_mange_tout_les: int = int(request.form['nb_de_nourriture_tout_les_predateur'])
-    loup_mange_combien: int = int(request.form['nb_de_nourriture_predateur'])
 
-    cerf_reproduction_tout_les: int = int(request.form['nb_bebe_tout_les_proie'])
-    cerf_reproduction_combien: list[int] = [int(request.form['nb_bebe_proie1']), int(request.form['nb_bebe_proie2'])]
-    cerf_mange_tout_les: int = int(request.form['nb_de_nourriture_tout_les_proie'])
-    cerf_mange_combien: int = int(request.form['nb_de_nourriture_proie'])
+    loup_reproduction_tout_les = int(request.form['nb_bebe_tout_les_preda'])
+    loup_reproduction_combien = [
+        int(request.form['nb_bebe_predateur1']),
+        int(request.form['nb_bebe_predateur2'])
+    ]
+    loup_mange_tout_les = int(request.form['nb_de_nourriture_tout_les_predateur'])
+    loup_mange_combien = int(request.form['nb_de_nourriture_predateur'])
 
-    herbe_reproduction_tout_les: int = int(request.form['nb_bebe_tout_les_vegetal'])
-    herbe_reproduction_combien: list[int] = [int(request.form['nb_bebe_vegetal1']), int(request.form['nb_bebe_vegetal2'])]
+    cerf_reproduction_tout_les = int(request.form['nb_bebe_tout_les_proie'])
+    cerf_reproduction_combien = [
+        int(request.form['nb_bebe_proie1']),
+        int(request.form['nb_bebe_proie2'])
+    ]
+    cerf_mange_tout_les = int(request.form['nb_de_nourriture_tout_les_proie'])
+    cerf_mange_combien = int(request.form['nb_de_nourriture_proie'])
+
+    herbe_reproduction_tout_les = int(request.form['nb_bebe_tout_les_vegetal'])
+    herbe_reproduction_combien = [
+        int(request.form['nb_bebe_vegetal1']),
+        int(request.form['nb_bebe_vegetal2'])
+    ]
 
     data = {
-        "loup" : {
-            "reproduction" : {"tout_les" : loup_reproduction_tout_les, "nombre_de_nv_nee" : loup_reproduction_combien, "maturiter_sexuel" : 2}, 
-            "mange" : {"qui" : "cerf", "tout_les" : loup_mange_tout_les, "combien" : loup_mange_combien}
+        "loup": {
+            "reproduction": {
+                "tout_les": loup_reproduction_tout_les,
+                "nombre_de_nv_nee": loup_reproduction_combien,
+                "maturiter_sexuel": 2
+            },
+            "mange": {
+                "qui": "cerf",
+                "tout_les": loup_mange_tout_les,
+                "combien": loup_mange_combien
+            }
         },
-
-        "cerf" : {
-            "reproduction" : {"tout_les" : cerf_reproduction_tout_les, "nombre_de_nv_nee" : cerf_reproduction_combien, "maturiter_sexuel" : 2},
-            "mange" : {"qui" : "herbe", "tout_les" : cerf_mange_tout_les, "combien" : cerf_mange_combien}
+        "cerf": {
+            "reproduction": {
+                "tout_les": cerf_reproduction_tout_les,
+                "nombre_de_nv_nee": cerf_reproduction_combien,
+                "maturiter_sexuel": 2
+            },
+            "mange": {
+                "qui": "herbe",
+                "tout_les": cerf_mange_tout_les,
+                "combien": cerf_mange_combien
+            }
         },
-
-        "herbe" : {
-            "reproduction" : {"tout_les" : herbe_reproduction_tout_les, "nombre_de_nv_nee" : herbe_reproduction_combien}
+        "herbe": {
+            "reproduction": {
+                "tout_les": herbe_reproduction_tout_les,
+                "nombre_de_nv_nee": herbe_reproduction_combien
+            }
         }
     }
 
@@ -111,10 +169,13 @@ def modifier():
         json.dump(data, f, ensure_ascii=False, indent=4)
 
     return render_template('index.html')
+
+
 @app.route("/credit")
 def credit():
     return render_template("credit.html")
     
+
 if __name__ == '__main__':
-    historique = {"loup" : [], "cerf" : [], "herbe" : []}
-    app.run(host = '127.0.0.1', port=5000, debug=True)
+    historique = {"loup": [], "cerf": [], "herbe": []}
+    app.run(host='127.0.0.1', port=5000, debug=True)
